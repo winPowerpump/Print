87d29ff19077498b265a5c43705ef56548931146
agorapilled <johngardner13@outlook.com>
2025-09-09 18:34:54 -0400

    v0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           <div className='w-full flex justify-center'>
             <Link
               href="/token"
-              className="px-4 py-2 text-gray-700 font-bold mb-2 text-lg"
+              className="px-4 py-2 text-gray-700 font-bold mb-2 text-xl"
             >
               [create]
             </Link>
