73322903d5699fc210f6ad0ecf1449ab4c0451e3
agorapilled <johngardner13@outlook.com>
2025-09-13 03:27:53 -0400

    v0

--- src/app/[addr]/page.js
+++ src/app/[addr]/page.js
@@ -185,21 +185,6 @@ export default function Token() {
                 <code className="flex-1 text-sm font-mono text-gray-200 px-3 py-2 rounded text-center">
                   {token.fee_account}
                 </code>
-                <button
-                  onClick={() => copyToClipboard(token.fee_account, 'fee')}
-                  className="p-2 text-gray-400 hover:text-white transition-colors"
-                  title="Copy fee account"
-                >
-                    {copiedField === 'fee' ? (
-                        <svg className="size-[20px] text-green-400" fill="currentColor" viewBox="0 0 20 20">
-                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                        </svg>
-                    ) : (
-                        <svg className="size-[20px] text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
-                        </svg>
-                    )}
-                </button>
               </div>
             </div>
           )}
